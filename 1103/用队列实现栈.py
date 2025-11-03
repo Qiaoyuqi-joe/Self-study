788990c85062153866716e914af9725b5b4e55fd
Qiaoyuqi-joe <jp2021212806@qmul.ac.uk>
2025-11-03 11:28:05 +0800

    1103

--- 1103/用队列实现栈.py
+++ 1103/用队列实现栈.py
@@ -31,3 +31,29 @@ class MyStack:
         return top_val
     def empty(self) -> bool:
         return not self.q1
+    
+
+
+
+    #-----------------------------------------------------------
+
+    #使用一个队列完成 把最后进去的数值留下 其他n-1个数值出队再入队  保证最后一个进来的数值在对头
+
+
+from collections import deque
+
+class MyStack:
+    def __init__(self):
+        self.q = deque()
+    def push(self,x:int) -> None:
+        self.q.append(x)
+        for _ in range (len(self.q)-1):
+            self.q.append(self.q.popleft())
+
+    def pop(self) -> int:
+        return self.q.popleft()  #直接这么写 是因为所有元素在入栈的时候已经经历过了push的调整了 所以直接就可以popleft（）
+    #出栈 直接弹出
+    def top(self) -> int:
+        return self.q[0]#只读取数值 不删除
+    def empty(self) -> bool:
+        return not self.q
